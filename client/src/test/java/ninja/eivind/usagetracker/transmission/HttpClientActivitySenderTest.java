5cf4bd76d64083446668cbc656e85cbc0ecfd170
Eivind Vegsundvåg <eivindveg@hotmail.com>
2016-03-16 22:28:40 +0100

    add failing unit test for httpclientactivitysender

---
+++ client/src/test/java/ninja/eivind/usagetracker/transmission/HttpClientActivitySenderTest.java
@@ -0,0 +1,19 @@
+package ninja.eivind.usagetracker.transmission;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HttpClientActivitySenderTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testSend() throws Exception {
+        fail("No preconditions for mocking a http server.");
+    }
+}
